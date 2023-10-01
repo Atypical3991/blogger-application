4da04619c69e873208626696c647cd9fc092d442
Biplab Dholey <biplab.dholey@indmoney.com>
2023-10-01 23:48:18 +0530

    nginx and uwsgi support added

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -134,3 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/generated/")
--- blog/blog/wsgi.py
+++ blog/blog/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
+
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+
+sys.path.append('/opt/homebrew/lib/python3.9/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
