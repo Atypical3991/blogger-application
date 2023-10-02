22c18dea3fb89312fa34ce905764681f3d216bab
Biplab Dholey <biplab.dholey@indmoney.com>
2023-10-02 20:45:44 +0530

    code refactored

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-0p99qj%38ov4u8v(17t@hx(b&0fax01r!wilkre#f(ns^s#(pi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.109.212.126']
 
 # Application definition
 
